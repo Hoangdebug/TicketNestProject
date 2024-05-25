31306c70f9674e62febd9bad376a63c7f2ab0ba1
theanhntp <theanhntp@gmail.com>
2024-05-25 19:37:26 +0700

    update seat DB

--- server/src/models/seat.ts
+++ server/src/models/seat.ts
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var ticketSchema = new mongoose.Schema({
-    cusname:{
+var seatSchema = new mongoose.Schema({
+    type:{
+        type:String,
+        required:true,
+    },
+    username:{
         user: {type:mongoose.Types.ObjectId, ref:'User'},
         type:String,
         required:true,
     },
-    eventname:{
-        event: {type:mongoose.Types.ObjectId, ref:'Event'},
-        type:String,
-        required:true,
-    },
     seatcode:{
+        seatcode: {type:mongoose.Types.ObjectId, ref:'Ticket'},
         type:String,
         required:true,
     },
-    Time:{
-        type:Date,
+    Status:{
+        type:Boolean,
         required:true,
     },
 });
 
 //Export the model
-module.exports = mongoose.model('Ticket', ticketSchema);
+module.exports = mongoose.model('Seat', seatSchema);
